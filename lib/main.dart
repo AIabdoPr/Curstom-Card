20b22e845f76a4d7001f051be273626ce7cd2cf3
Abdo Pr <55705726+AIabdoPr@users.noreply.github.com>
2021-08-14 17:43:18 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:custom_card/views/bottom_drawer_head_view.dart';
 import 'package:custom_card/views/my_card_view.dart';
+import 'package:bottom_drawer/bottom_drawer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,34 +28,96 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.red[900],
-        appBar: AppBar(
-            title: Center(child: Text("Tickets")),
-            backgroundColor: Colors.red[900]),
-        body: Center(
-          child: CarouselSlider.builder(
-            itemCount: 15,
-            itemBuilder:
-                (BuildContext context, int itemIndex, int pageViewIndex) =>
-                    MyCardView(
-              width: MediaQuery.of(context).size.width * 0.6,
-              height: MediaQuery.of(context).size.height * 0.78,
+      backgroundColor: Colors.red[900],
+      appBar: AppBar(
+          title: Center(child: Text("Tickets")),
+          backgroundColor: Colors.red[900]),
+      body: Center(
+        child: Stack(
+          children: [
+            CarouselSlider.builder(
+              itemCount: 15,
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      MyCardView(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: MediaQuery.of(context).size.height * 0.78,
+              ),
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.78,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                onPageChanged: (index, reason) {},
+                scrollDirection: Axis.horizontal,
+              ),
             ),
-            options: CarouselOptions(
-              height: MediaQuery.of(context).size.height * 0.78,
-              aspectRatio: 16 / 9,
-              viewportFraction: 0.8,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: true,
-              onPageChanged: (index, reason) {},
-              scrollDirection: Axis.horizontal,
-            ),
+            _buildBottomDrawer(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _button = 'None';
+  double _headerHeight = 70;
+  double _bodyHeight = 500;
+  BottomDrawerController _controller = BottomDrawerController();
+
+  Widget _buildBottomDrawer(BuildContext context) {
+    return BottomDrawer(
+      header: _buildBottomDrawerHead(context),
+      body: _buildBottomDrawerBody(context),
+      headerHeight: _headerHeight,
+      drawerHeight: _bodyHeight,
+      color: Colors.transparent,
+      controller: _controller,
+    );
+  }
+
+  Widget _buildBottomDrawerHead(BuildContext context) {
+    return Container(
+      height: _headerHeight,
+      child: Center(child: BottomDrawerHeadView(_controller)),
+    );
+  }
+
+  Widget _buildBottomDrawerBody(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: _bodyHeight,
+      color: Color(0XFFBB1C1C),
+      child: SingleChildScrollView(
+        child: Column(
+          children: _buildButtons('Body', 1, 25),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildButtons(String prefix, int start, int end) {
+    List<Widget> buttons = [];
+    for (int i = start; i <= end; i++)
+      buttons.add(TextButton(
+        child: Text(
+          '$prefix Button $i',
+          style: TextStyle(
+            fontSize: 15.0,
+            color: Colors.black,
           ),
-        ));
+        ),
+        onPressed: () {
+          setState(() {
+            _button = '$prefix Button $i';
+          });
+        },
+      ));
+    return buttons;
   }
 }
